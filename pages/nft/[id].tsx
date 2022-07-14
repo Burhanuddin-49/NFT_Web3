6b400db76ef200bd802261bfd52ab13fda9e76de
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-07-15 03:05:02 +0530

    header & content done

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -23,10 +23,36 @@ function NFTDropPage() {
       </div>
 
       {/* Right */}
-      <div>
+      <div className="flex flex-1 flex-col p-12 lg:col-span-6">
         {/* Header */}
+        <header className="flex items-center justify-between">
+          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+            The{" "}
+            <span className="font-extrabold underline decoration-pink-600/50">
+              PAPAFAM
+            </span>{" "}
+            NFT Market Place
+          </h1>
+
+          <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">
+            Sign In
+          </button>
+        </header>
+
+        <hr className="my-2 border" />
 
         {/* Content */}
+        <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
+          <img
+            className="w-80 object-cover pb-10 lg:h-40"
+            src="https://links.papareact.com/bdy"
+            alt=""
+          />
+          <h1 className="text-3xl font-bold lg:text-4xl lg:font-extrabold">
+            The PAPAFAM Ape Coding Club | NFT Drop
+          </h1>
+          <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
+        </div>
 
         {/* Mint Button */}
       </div>
