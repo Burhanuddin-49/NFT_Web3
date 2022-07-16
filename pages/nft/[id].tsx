41f8c34d0c45a9ff95d13b934e887b9e64129456
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-07-17 02:48:46 +0530

    signin and signout page done

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 
 function NFTDropPage() {
+  // Auth
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const disconnect = useDisconnect();
+  //--
+
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/* Left */}
@@ -34,12 +41,21 @@ function NFTDropPage() {
             NFT Market Place
           </h1>
 
-          <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">
-            Sign In
+          <button
+            onClick={() => (address ? disconnect() : connectWithMetamask())}
+            className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base"
+          >
+            {address ? "Sign Out" : "Sign In"}
           </button>
         </header>
 
         <hr className="my-2 border" />
+        {address && (
+          <p className="text-center text-sm text-rose-400">
+            You're logged in with wallet {address.substring(0, 5)}...
+            {address.substring(address.length - 5)}
+          </p>
+        )}
 
         {/* Content */}
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
@@ -51,7 +67,9 @@ function NFTDropPage() {
           <h1 className="text-3xl font-bold lg:text-4xl lg:font-extrabold">
             The PAPAFAM Ape Coding Club | NFT Drop
           </h1>
-          <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
+          <p className="pt-2 text-xl text-green-500 lg:pt-3">
+            13 / 21 NFT's claimed
+          </p>
         </div>
 
         {/* Mint Button */}
